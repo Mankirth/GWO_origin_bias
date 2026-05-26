31ab3f5a1e47284adcb07f81ce3148da61ecfa65
Mankirth <159180334+Mankirth@users.noreply.github.com>
2026-05-26 18:05:30 -0400

    PSO(M) results updated

--- EvoloPy/optimizer.py
+++ EvoloPy/optimizer.py
@@ -22,7 +22,7 @@ import EvoloPy.optimizers.JAYA as jaya
 import EvoloPy.optimizers.DE as de
 import EvoloPy.optimizers.DE_modified as de_modified
 import EvoloPy.optimizers.GWO_modified as gwo_modified
-from EvoloPy import CEC2022 as benchmarks
+from EvoloPy import benchmarks
 import csv
 import numpy
 import time
